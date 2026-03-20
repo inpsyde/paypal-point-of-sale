08eddbc283d308e4f260e117803d7bfc080bd87b
Daniel Dudzic <d.dudzic@syde.com>
2026-03-20 18:06:22 +0100

    🔌 Migrate paypal-pos-product-debug to inpsyde/modularity (Phase 3)

--- modules.local/paypal-pos-product-debug/module.php
+++ modules.local/paypal-pos-product-debug/module.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Syde\PayPal\PointOfSale\ProductDebug;
 
-use Dhii\Modular\Module\ModuleInterface;
-
-return static function (): ModuleInterface {
+return static function (): ProductDebugModule {
     return new ProductDebugModule();
 };
