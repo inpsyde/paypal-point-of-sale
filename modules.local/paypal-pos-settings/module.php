b26a215b0765a869ee012245da01572a95f0e99a
Daniel Dudzic <d.dudzic@syde.com>
2026-03-20 18:07:16 +0100

    🔌 Migrate paypal-pos-settings to inpsyde/modularity (Phase 3)

--- modules.local/paypal-pos-settings/module.php
+++ modules.local/paypal-pos-settings/module.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Syde\PayPal\PointOfSale\Settings;
 
-use Dhii\Modular\Module\ModuleInterface;
-
-return static function (): ModuleInterface {
+return static function (): SettingsModule {
     return new SettingsModule();
 };
