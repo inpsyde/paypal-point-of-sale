584ac4896b99bf656e8a5fbaf0efa7574359015e
Daniel Dudzic <d.dudzic@syde.com>
2026-03-20 18:02:05 +0100

    🔌 Migrate paypal-pos-auth to inpsyde/modularity (Phase 2)

--- modules.local/paypal-pos-auth/module.php
+++ modules.local/paypal-pos-auth/module.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Syde\PayPal\PointOfSale\Auth;
 
-use Dhii\Modular\Module\ModuleInterface;
-
-return static function (): ModuleInterface {
+return static function (): AuthModule {
     return new AuthModule();
 };
