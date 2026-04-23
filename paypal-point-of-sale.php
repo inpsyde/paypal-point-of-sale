68e555263c01c207559c29857cf74440c7ac38b5
inpsyde-deploybot <github-deploybot@inpsyde.com>
2026-04-23 12:41:42 +0000

    [BOT] Add build artifact from dev/IZET-522-phpstan -> IZET-522-phpstan.

--- paypal-point-of-sale.php
+++ paypal-point-of-sale.php
@@ -6,7 +6,7 @@ declare (strict_types=1);
  * Plugin Name: PayPal Point of Sale
  * Plugin URI:  https://zettle.inpsyde.com/
  * Description: PayPal Point of Sale Integration for WooCommerce
- * Version: 0.0.0+izet-522-phpstan.6b6a053
+ * Version: 0.0.0+izet-522-phpstan.40e1064
  * Requires at least: 6.8
  * Requires PHP: 8.2
  * Requires Plugins: woocommerce
