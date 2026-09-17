602e002fb7a521341e288fa00f4a7d6e5757a529
inpsyde-deploybot <github-deploybot@inpsyde.com>
2026-09-17 07:04:06 +0000

    [BOT] Add build artifact from dev/POS-675-qit-security -> POS-675-qit-security.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -35,8 +35,8 @@ class ComposerAutoloaderInit90f41f063576f9e0b2fc8af17b0983fd
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit90f41f063576f9e0b2fc8af17b0983fd::$files;
         $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files_796436804b48f12ef4c7b67e3868213c5f3a102b'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files_796436804b48f12ef4c7b67e3868213c5f3a102b'][$fileIdentifier] = true;
+            if (empty($GLOBALS['__composer_autoload_files_f603d30ef09ff70482977ac702685707b568be8c'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files_f603d30ef09ff70482977ac702685707b568be8c'][$fileIdentifier] = true;
 
                 require $file;
             }
--- zettle-pos-integration.php
+++ zettle-pos-integration.php
@@ -6,8 +6,8 @@ declare (strict_types=1);
  * Plugin Name: PayPal Point of Sale
  * Plugin URI:  https://zettle.inpsyde.com/
  * Description: PayPal Point of Sale Integration for WooCommerce
- * Version: 2.0.0+pos-675-qit-security.7964368
- * SHA: 796436804b48f12ef4c7b67e3868213c5f3a102b
+ * Version: 2.0.0+pos-675-qit-security.f603d30
+ * SHA: f603d30ef09ff70482977ac702685707b568be8c
  * Requires at least: 6.8
  * Requires PHP: 8.2
  * Requires Plugins: woocommerce
